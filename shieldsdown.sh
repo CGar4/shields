95ba08f70427d1a0fdfcecdb84553ed3d7acc0e4
los <carlosg4252@gmail.com>
2019-10-22 17:34:28 -0500

    changed file location for easy access

--- shieldsdown.sh
+++ shieldsdown.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo killall openvpn && sleep 2 && sudo iptables-restore -v < ~/shields/firewallrules/default-iptables-ip4 && sleep 2 && sudo python ~/shields/tor/toriptables2-master/toriptables2.py -f
-cat ~/shields/art/downart.txt
+sudo killall openvpn && sleep 2 && sudo iptables-restore -v < 
+/etc/shields/firewallrules/default-iptables-ip4 && sleep 2 && sudo python 
+/etc/shields/tor/toriptables2-master/toriptables2.py -f
+cat /etc/shields/art/downart.txt
--- shieldsup.sh
+++ shieldsup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo -b openvpn ~/shields/vpn-config/protonvpn.com.udp.ovpn && sleep 10 && sudo python ~/shields/tor/toriptables2-master/toriptables2.py -l
-cat ~/shields/art/upart.txt
+sudo -b openvpn /etc/shields/vpn-config/protonvpn.com.udp.ovpn && sleep 10 && sudo 
+python /etc/shields/tor/toriptables2-master/toriptables2.py -l
+cat /etc/shields/art/upart.txt
